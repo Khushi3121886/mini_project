0f157807e0781c30689ef928b040bc7566f59090
Khushi3121886 <98952356+Khushi3121886@users.noreply.github.com>
2025-03-18 14:15:58 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="dagsterflightpricerepo",
+    name="mini_project",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
